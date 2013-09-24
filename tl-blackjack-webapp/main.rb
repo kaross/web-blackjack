dd09af3f25346b1575ca02f7f9b524dac940362e
Kent A Ross <kaross@gmail.com>
2013-09-24 17:32:51 -0400

    added betting

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 
 set :sessions, true
 
@@ -25,11 +26,40 @@ helpers do
 
     sum
   end
+
+  def show_card(card)
+    case card[0]
+    when 'C' 
+      suit = 'clubs'
+    when 'D' 
+      suit = 'diamonds'
+    when 'H' 
+      suit = 'hearts'
+    else 
+      suit = 'spades'
+    end
+
+    case card[1]
+      when 'A' 
+        face = 'ace'
+      when 'J' 
+        face = 'jack'
+      when 'Q' 
+        face = 'queen'
+      when 'K' 
+        face = 'king'
+      else 
+        face = card[1].to_i
+    end
+
+    "<img src='/images/cards/#{suit}_#{face}.jpg' class='image-rounded'>"
+  end
 end
 
 get '/' do
   if session[:username]
-    redirect '/game'
+    session[:balance] = 500
+    redirect '/take_bet'
   else
     redirect '/get_name'
   end
@@ -40,7 +70,32 @@ get '/get_name' do
 end
 
 post '/store_name' do
+  if params['username'].empty? || params['username'].to_i != 0
+    @error = "Must enter a name"
+    halt erb :get_name
+  end
+
   session[:username] = params['username']
+  session[:balance] = 500
+  redirect '/take_bet'
+end
+
+get '/take_bet' do
+  if session[:balance] < 5
+    redirect '/quit'
+  end
+
+  erb :take_bet
+end
+
+post '/take_bet' do
+  if params['bet'].empty? || params['bet'].to_i < 5
+    @error = "Please enter a valid bet"
+    halt erb :take_bet
+  end
+  
+  session[:bet] = params['bet'].to_i
+  session[:balance] -= session[:bet]
   redirect '/game'
 end
 
@@ -57,9 +112,30 @@ get '/game' do
     session[:player_cards] << session[:deck].pop
   end
 
-#  session[:player_cards] = [['S', 'A'],['S','J']]
+# session[:player_cards] = [['S', 'A'],['S','J']]
 
-  @show_player_options = true
+  # My understanding is that the automatic win rules for hitting 21 only apply during the first
+  # round before any hits, so do it here.
+  # For simplicity, we just give player blackjack to the player regardless of the dealer.
+  if best_value(session[:player_cards]) == 21
+    session[:player_stay] = true
+    @show_player_options = false
+    @show_dealer_turn = false
+    @show_play_again = true
+    @success = 'Congratulations, you hit Blackjack!'
+    session[:balance] += (session[:bet] * 2.5).round
+  elsif best_value(session[:dealer_cards]) == 21
+    session[:player_stay] = true
+    @show_player_options = false
+    @show_dealer_turn = false
+    @show_play_again = true
+    @error = 'Dealer Blackjack! Sorry, you lose.'
+  else
+    session[:player_stay] = false
+    @show_player_options = true
+    @show_dealer_turn = false
+  end 
+
   erb :game
 end
 
@@ -68,8 +144,10 @@ post '/hit' do
   if best_value(session[:player_cards]) > 21
     @error = "Sorry, you busted on this hand."
     @show_player_options = false
+    @show_dealer_turn = false
   else 
     @show_player_options = true
+    @show_dealer_turn = false
   end
 
   erb :game
@@ -77,6 +155,57 @@ end
 
 post '/stay' do
   @msg = "You've chosen to stay, it is the dealer's turn."
+  session[:player_stay] = true
+
+  redirect '/dealer_turn'
+end
+
+get '/dealer_turn' do
+  if best_value(session[:dealer_cards]) > 21
+    @dealer_status = "and has busted!"
+  elsif best_value(session[:dealer_cards]) < 17
+    @dealer_status = "and will hit"
+  else
+    @dealer_status = "and will stay"
+  end
+
+  @show_dealer_turn = true
   @show_player_options = false
   erb :game
 end
+
+post '/dealer_turn' do
+  if best_value(session[:dealer_cards]) < 17
+    session[:dealer_cards] << session[:deck].pop
+    redirect '/dealer_turn'
+  else
+    redirect '/results'
+  end
+end
+
+get '/results' do
+  @show_player_options = false
+  @show_dealer_turn = false
+
+  # Player bust was dealt with during player turn.
+  if best_value(session[:dealer_cards]) > 21 
+    @success = "Congratulations, you won!"
+    session[:balance] += session[:bet] * 2
+  elsif best_value(session[:dealer_cards]) > best_value(session[:player_cards])
+    @error = "Sorry, the dealer wins!"
+  elsif best_value(session[:dealer_cards]) < best_value(session[:player_cards])
+    @success = "Congratulations, you won!"
+    session[:balance] += session[:bet] * 2
+  else
+    @msg = "The hands are equal - push!"
+    session[:balance] += session[:bet]    
+  end
+
+  @show_play_again = true
+  erb :game
+end
+
+get '/quit' do
+  erb :quit
+end
+
